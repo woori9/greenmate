2eee8730b32724529d770acfc37b1bad6b4502eb
yujin won <won_yujin@naver.com>
2022-04-28 21:55:49 +0900

    feat: 🎸 [front] 모임 생성 폼 구현중

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MyMoim from './routes/MyMoim';
+import MoimForm from './routes/MoimForm';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/mymoim" element={<MyMoim />} />
+        <Route path="/mymoim/create" element={<MoimForm />} />
       </Routes>
     </BrowserRouter>
   );
