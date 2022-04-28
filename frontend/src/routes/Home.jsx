75e70cc40ca715a2a2d339c5112dccf5057eb6ef
syc1008 <syc1008@naver.com>
2022-04-28 22:58:39 +0900

    style: 💄 [front] 모바일, 데스크탑 네브바 스타일링

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,11 +1,16 @@
+import styled from 'styled-components';
 import ResponsiveNavbar from '../components/common/navbar/ResponsiveNavbar';
+
+const Check = styled.div`
+  border: 1px solid red;
+`;
 
 function Home() {
   return (
-    <>
-      <h1>홈</h1>
+    <Check>
       <ResponsiveNavbar />
-    </>
+      <h1>그린메이트그린메이트그린메이트그린메이트그린메이트그린메이트</h1>
+    </Check>
   );
 }
 
