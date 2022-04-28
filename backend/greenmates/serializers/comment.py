bdf30691933faf95f23e2a02e4f9ec9dde8049ca
최시열 <continue75@naver.com>
2022-04-28 02:34:31 +0000

    Feature/커뮤니티 siyeol

--- backend/greenmates/serializers/comment.py
+++ backend/greenmates/serializers/comment.py
@@ -1,14 +1,6 @@
 from rest_framework import serializers
 from ..models import Comment
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-class UserSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = User
-        fields = ('id', 'username')
+from accounts.serializers import UserSerializer
 
 
 class CommentSerializer(serializers.ModelSerializer):
