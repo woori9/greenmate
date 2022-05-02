37b4f068d216695e8c69d54bf01c7a3f23a33239
yujin won <won_yujin@naver.com>
2022-05-02 16:55:23 +0900

    refactor: 💡 [front] home에서 모임 카테고리 null 값 할당
    홈에서 모임 상세 페이지로 이동하는 경우 카테고리 처리 목적

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,12 +1,21 @@
 import styled from 'styled-components';
+import { useEffect } from 'react';
+import { useAtom } from 'jotai';
 import ResponsiveNavbar from '../components/common/navbar/ResponsiveNavbar';
 import FloatingActionBtn from '../components/common/FloatingActionBtn';
+import moim from '../atoms/moim';
 
 const Container = styled.div`
   border: 1px solid red;
 `;
 
 function Home() {
+  const [, setSelectedCategory] = useAtom(moim);
+
+  useEffect(() => {
+    setSelectedCategory(null);
+  }, []);
+
   return (
     <Container>
       <ResponsiveNavbar />
