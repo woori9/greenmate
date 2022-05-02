1450406e6b89ba798b012c1227f6db74c37aab50
yujin won <won_yujin@naver.com>
2022-05-02 16:51:45 +0900

    refactor: 💡 [front] 파일명 변경 및 moimInfo 타입 상세 검사

--- frontend/src/routes/MyMoim.jsx
+++ frontend/src/routes/MyMoim.jsx
@@ -1,9 +1,10 @@
 import styled from 'styled-components';
+import { useEffect } from 'react';
 import { useAtom } from 'jotai';
 import ResponsiveNavbar from '../components/common/navbar/ResponsiveNavbar';
 import FloatingActionBtn from '../components/common/FloatingActionBtn';
 import MoimCategory from '../components/moim/MoimCategory';
-import MoimInfo from '../components/moim/MoimInfo';
+import MoimCard from '../components/moim/MoimCard';
 import moim from '../atoms/moim';
 
 const CategoryDiv = styled.ul`
@@ -17,6 +18,12 @@ function MyMoim() {
   const [selectedCategory, setSelectedCategory] = useAtom(moim);
   const moimCategories = ['대기', '참여', '진행', '완료'];
 
+  useEffect(() => {
+    if (selectedCategory === null) {
+      setSelectedCategory(0);
+    }
+  }, []);
+
   const data = [
     {
       id: 1,
@@ -28,19 +35,26 @@ function MyMoim() {
       author: {
         id: 1,
         nickname: '김싸피',
-        vegeType: '락토오보',
+        vegeType: 3,
       },
       title: '야채 식당 가실 분!',
-      content: '이번주에 야채 식당 가실 분 구해요',
-      time: new Date(),
+      content:
+        '제주 여행 중인데 야채 식당 가보고 싶어서 글 올려요! 여러 메뉴 시켜서 같이 맛보면 너무 좋을 것 같아요',
+      time: new Date('2022-06-02T19:35:00'),
       headCnt: 4,
       nowCnt: 2,
       status: 0,
       mates: [
         {
           userId: 2,
-          userNickname: '이이이',
-          vegeType: '락토',
+          nickname: '이이이',
+          vegeType: 4,
+          mateStatus: 1,
+        },
+        {
+          userId: 3,
+          nickname: '박박박',
+          vegeType: 2,
           mateStatus: 1,
         },
       ],
@@ -52,9 +66,11 @@ function MyMoim() {
         nameKr: '채소 식당',
         addressKr: '대전시 유성구 oo동',
       },
-      author: 1,
-      authorNickname: '김싸피',
-      authorVegeType: '락토오보',
+      author: {
+        id: 2,
+        nickname: '박싸피',
+        vegeType: 4,
+      },
       title: '야채 식당 가실 분!',
       content: '이번주에 야채 식당 가실 분 구해요',
       time: new Date(),
@@ -63,9 +79,9 @@ function MyMoim() {
       status: 0,
       mates: [
         {
-          userId: 2,
-          userNickname: '빅빅빅',
-          vegeType: '락토',
+          userId: 4,
+          nickname: '최싸피',
+          vegeType: 3,
           mateStatus: 1,
         },
       ],
@@ -89,7 +105,7 @@ function MyMoim() {
       </CategoryDiv>
       <hr />
       {data.map(moimInfo => (
-        <MoimInfo
+        <MoimCard
           key={moimInfo.id}
           moimInfo={moimInfo}
           hasBorder={false}
