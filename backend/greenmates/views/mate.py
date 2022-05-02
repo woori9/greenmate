43e5b370b63fa86c186d02c3c36038a1af9339e2
박소미 <psomi0114@gmail.com>
2022-05-02 02:02:27 +0000

    Feature/모임 somi

---
+++ backend/greenmates/views/mate.py
@@ -0,0 +1,131 @@
+from django.shortcuts import get_list_or_404, get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from uritemplate import partial
+from ..models import Moim, Mate
+from ..serializers.moim import MoimSerializer
+from ..serializers.mate import (
+    MatePutPostSerializer
+)
+
+from rest_framework.status import (
+    HTTP_201_CREATED,
+    HTTP_204_NO_CONTENT,
+    HTTP_403_FORBIDDEN,
+    HTTP_405_METHOD_NOT_ALLOWED,
+    HTTP_409_CONFLICT,
+)
+import datetime
+from django.contrib.auth import get_user_model
+User = get_user_model()
+user = User.objects.get(pk=2) 
+
+@api_view(['POST'])
+def apply_mate(request, moim_id):
+    '''
+    POST: 해당 모임에 대기 신청
+    ''' 
+    moim = get_object_or_404(Moim, pk=moim_id)
+    user_in_mate = moim.mate_set.values('user').filter(user=user.pk)
+    if len(user_in_mate): # 호스트 or 이전에 거절/합류 된 유저라면 대기 신청 못함
+        return Response(
+            data='이 모임에 대기 신청이 불가합니다.',
+            status=HTTP_409_CONFLICT
+        )
+
+    serializer = MatePutPostSerializer(data={'moim': moim_id, 'user': user.pk, 'mate_staus': 0})
+    if serializer.is_valid(raise_exception=True):
+        serializer.save()
+        return Response(
+            data=f'{moim_id}번 모임에 대기 신청되었습니다.',
+            status=HTTP_201_CREATED
+        )
+
+@api_view(['DELETE'])
+def cancle_mate(request, mate_id):
+    '''
+    DELETE: 게스트가 대기 신청을 취소
+    '''
+    mate = get_object_or_404(Mate, pk=mate_id)
+    if mate.user.pk != user.pk: # 대기 신청자 본인이 아닌 경우
+        return Response(
+            data='접근 권한이 없습니다.',
+            status=HTTP_403_FORBIDDEN
+        )
+    if mate.mate_status != 0: # 대기 상태가 아닌 경우 (대기 취소가 아닌, 모임 나가기 해야 함)
+        return Response(
+            data='대기 취소가 불가합니다.',
+            status=HTTP_409_CONFLICT
+        )
+
+    mate.delete()
+    return Response(
+        data='대기 신청이 정상적으로 취소되었습니다.',
+        status=HTTP_204_NO_CONTENT
+    )
+
+@api_view(['PUT'])
+def accept_mate(request, mate_id):
+    '''
+    PUT: 해당 모임의 호스트가 게스트의 참여를 수락
+    '''
+    mate = get_object_or_404(Mate, pk=mate_id)
+    moim = mate.moim
+    if moim.author.pk != 3: # 모임의 호스트가 아닌 경우
+        return Response(
+            data='접근 권한이 없습니다.',
+            status=HTTP_403_FORBIDDEN
+        )
+    if mate.mate_status != 0 or moim.status != 0: # 게스트가 대기 상태가 아닌 경우거나 모임 모집중이 아닌 경우
+        return Response(
+            data='참여 수락이 불가합니다.',
+            status=HTTP_409_CONFLICT
+        )
+    mate.mate_status = 1
+    mate.save()
+    moim_serializer = MoimSerializer(moim).data
+    if moim_serializer['now_cnt'] == moim_serializer['head_cnt']:
+        moim.status = 1
+        moim.save()
+    return Response(
+        data='게스트 참여를 수락했습니다.',
+        status=HTTP_201_CREATED
+    )
+
+@api_view(['PUT'])
+def decline_mate(request, mate_id):
+    '''
+    PUT: 호스트가 게스트의 참여를 거절
+    '''
+    mate = get_object_or_404(Mate, pk=mate_id)
+    moim = mate.moim
+    if moim.author.pk != user.pk: # 모임의 호스트가 아닌 경우
+        return Response(
+            data='접근 권한이 없습니다.',
+            status=HTTP_403_FORBIDDEN
+        )
+    if mate.mate_status != 0: # 게스트가 대기 상태가 아닌 경우
+        return Response(
+            data='참여 거절이 불가합니다.',
+            status=HTTP_409_CONFLICT
+        )
+    mate.mate_status = 2
+    mate.save()
+    return Response(
+        data='게스트 참여를 거절했습니다.',
+        status=HTTP_201_CREATED
+    )
+
+@api_view(['DELETE'])
+def out_mate(request, mate_id):
+    '''
+    DELETE: 모임 나가기
+      case1: 참여 수락된 게스트가 모임 나감
+      case2: 호스트가 모임 2시간이 되기 전에 모임을 취소
+      case3: 모임 2시간 전까지 인원 모집이 안됨
+    '''
+    pass
+
+@api_view(['PUT'])
+def evaluate_mate(request, moim_id):
+    pass
--- backend/greenmates/views/moim.py
+++ backend/greenmates/views/moim.py
@@ -4,19 +4,23 @@ from rest_framework.response import Response
 from django.db.models import Prefetch
 from ..models import Moim, Mate
 from ..serializers.moim import (
-    MoimSimpleKrSerializer,
-    MoimSimpleEnSerializer,
-    MoimDetailKrSerializer, 
-    MoimDetailEnSerializer,
-    MoimAllKrSerializer,
-    MoimAllEnSerializer,
+    MoimSimpleSerializer,
+    MoimDetailSerializer, 
+    MoimAllSerializer,
     MoimTransSerializer,
     MoimPostPutSerializer,
 )
 
+from rest_framework.status import (
+    HTTP_201_CREATED,
+    HTTP_204_NO_CONTENT,
+    HTTP_403_FORBIDDEN,
+    HTTP_405_METHOD_NOT_ALLOWED,
+)
+import datetime
 from django.contrib.auth import get_user_model
 User = get_user_model()
-user = User.objects.filter(pk=2)   # TODO: request.user로 변경 (현재 pk=2로 TEST중)
+user = User.objects.get(pk=2)   # TODO: request.user로 변경 (현재 pk=2로 TEST중)
 # TODO: 유저정보
 # 1. request.user 언어정보
 # 2. login_required
@@ -30,19 +34,25 @@ def get_create_moim_list(request):
     '''
     def moim_list():
         moims = Moim.objects.filter(status=0)
-        serializer = MoimSimpleKrSerializer(moims, many=True)
-        # if user.language: # En
-        #     serializer = MoimSimpleEnSerializer(moims, many=True)
+        serializer = MoimSimpleSerializer(moims, many=True)
         return Response(serializer.data)
 
     def moim_create():
-        pass
+        serializer = MoimPostPutSerializer(data=request.data, context={'user': user}) 
+        author = user
+        content_trans = 'English version of context' # TODO: community 번역 함수 활용예정
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(author=author, content_trans=content_trans)
+        return Response(
+            data='모임이 정상적으로 작성되었습니다.',
+            status=HTTP_201_CREATED
+        )
     
     if request.method == 'GET':
         return moim_list()
 
     elif request.method =="POST":
-        moim_create()
+        return moim_create()
 
 @api_view(['GET', 'PUT'])
 def get_update_moim_detail(request, moim_id):
@@ -51,30 +61,44 @@ def get_update_moim_detail(request, moim_id):
     PUT : 해당 모임 글의 날짜, 시간 수정 (2시간 전까지)
     '''
     def moim_detail():
-        moim = get_object_or_404(Moim, pk=moim_id)
-        serializer = MoimDetailKrSerializer(moim)
-        # if user.language: # En
-        #     serializer = MoimDetailEnSerializer(moim)
+        serializer = MoimDetailSerializer(moim)
         return Response(serializer.data)
 
     def moim_update():
-        pass
+        # request.user가 호스트인지 확인
+        if moim.author != user:
+            return Response(
+                data='약속 시간 수정 권한이 없습니다.',
+                status=HTTP_403_FORBIDDEN
+            )
+        # 2시간 전인지 확인
+        two_hrs = datetime.datetime.now() + datetime.timedelta(hours=2)
+        appointment = moim.time
+        if two_hrs >= appointment:
+            return Response(
+                data='약속 시간 수정이 불가합니다.',
+                status=HTTP_405_METHOD_NOT_ALLOWED
+            )
+        serializer = MoimPostPutSerializer(moim, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
     
+    moim = get_object_or_404(Moim, pk=moim_id)
     if request.method == 'GET':
         return moim_detail()
     
     elif request.method == 'PUT':
-        moim_update()
+        return moim_update()
 
 @api_view(['GET'])
 def get_trans_moim(request, moim_id):
     '''
     GET: 해당 모임글의 번역글을 조회
     '''
-    if request.method == 'GET':
-        moim = get_object_or_404(Moim, pk=moim_id)
-        serializer = MoimTransSerializer(moim)
-        return Response(serializer.data)
+    moim = get_object_or_404(Moim, pk=moim_id)
+    serializer = MoimTransSerializer(moim)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def get_waiting_moim(request):
@@ -82,46 +106,33 @@ def get_waiting_moim(request):
     GET: 유저가 게스트로 대기중인 모임 리스트 조회
     '''
     # TODO: author_id를 request.user에서 가져오기. 현재 2번 user로 TEST 중.
-    if request.method == 'GET':
-        moims = get_list_or_404(Moim.objects.filter(mate__user=2, mate__mate_status=0).exclude(author_id=2))
-        serializer = MoimDetailKrSerializer(moims, many=True)
-        # if user.language:
-        #     serializer = MoimDetailEnSerializer(moims)
-        return Response(serializer.data)
+    moims = get_list_or_404(Moim.objects.filter(mate__user=2, mate__mate_status=0))
+    serializer = MoimDetailSerializer(moims, many=True)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def get_joined_moim(request):
     '''
     GET: 유저가 게스트로 합류중인 모임 리스트 조회
     ''' 
-    if request.method == 'GET':
-        moims = get_list_or_404(Moim.objects.filter(mate__user=2, mate__mate_status=1).exclude(author_id=2))
-        serializer = MoimDetailKrSerializer(moims, many=True)
-        # if user.language:
-        #     serializer = MoimDetailEnSerializer(moims)
-        return Response(serializer.data)
+    moims = get_list_or_404(Moim.objects.filter(mate__user=2, mate__mate_status=1).exclude(author_id=2))
+    serializer = MoimDetailSerializer(moims, many=True)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def get_opened_moim(request):
     '''
     GET: 유저가 호스트로 개설한 모임 리스트 조회
     '''
-    if request.method == 'GET':
-        moims = get_list_or_404(Moim.objects.filter(author_id=2, status__lt=2))
-        serializer = MoimAllEnSerializer(moims, many=True)
-        # if user.language:
-        #     serializer = MoimAllKrSerializer(moims, many=True)
-            
-        return Response(serializer.data)
+    moims = get_list_or_404(Moim.objects.filter(author_id=2, status__lt=2))
+    serializer = MoimAllSerializer(moims, many=True) 
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def get_finished_moim(request):
     '''
     GET: 유저가 호스트 / 게스트로 참여한 종료된 모임 리스트 조회
     '''
-    if request.method == 'GET':
-        moims = get_list_or_404(Moim.objects.filter(mate__user=2, mate__mate_status=4))
-        serializer = MoimDetailKrSerializer(moims, many=True)
-        # if user.language:
-        #     serializer = MoimDetailEnSerializer(moims)
-        return Response(serializer.data)
+    moims = get_list_or_404(Moim.objects.filter(mate__user=2, mate__mate_status=4))
+    serializer = MoimDetailSerializer(moims, many=True)
+    return Response(serializer.data)
