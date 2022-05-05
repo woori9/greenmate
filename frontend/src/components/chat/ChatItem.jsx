5a0b2fa0bf70d732ea9228d0df7a8fd9871e7ff2
woori9 <wrh95222@gmail.com>
2022-05-05 15:35:18 +0900

    style: 💄 [front] ChatItem css 추가

--- frontend/src/components/chat/ChatItem.jsx
+++ frontend/src/components/chat/ChatItem.jsx
@@ -1,15 +1,67 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { formatChatDateTime } from '../../utils/formattedDate';
 
 const StyledChatItem = styled.li`
-  margin: 0;
+  display: grid;
+  grid-template-columns: 1.5fr 5fr 2fr;
+  width: 100%;
+  padding: 0.5rem 1rem;
+  min-width: 200px;
+  column-gap: 4%;
+  border: gray 1px;
+
+  img {
+    max-width: 100%;
+    height: auto;
+    border-radius: 50%;
+    background-color: azure;
+    grid-row: 1 / 3;
+    margin: auto;
+  }
+
+  .nickname {
+    font-size: 20px;
+    font-weight: 500;
+  }
+
+  .color-gray {
+    color: #a9a9a9;
+  }
+
+  .data-unread {
+    display: inline-block;
+    background-color: #f9795d;
+    color: white;
+    border-radius: 50%;
+    min-width: 20px;
+    text-align: center;
+  }
+
+  .text-right {
+    text-align: right;
+  }
+
+  .m-5 {
+    margin: 5px;
+  }
 `;
 
-function ChatItem({ chat, onChatClick }) {
+function ChatItem({ chat, onChatClick, user, countUnreadMessage }) {
+  const { membersInfo, recentMessage } = chat;
+  const time = formatChatDateTime(recentMessage.sentAt);
+  const pair = membersInfo.find(member => member.id !== user);
   return (
     <StyledChatItem onClick={() => onChatClick(chat.id)}>
       <img src={`${process.env.PUBLIC_URL}/logo192.png`} alt="sample" />
-      CHAT ITEM
+      <p className="nickname">{pair.nickname}</p>
+      <p className="color-gray text-right m-5">{time}</p>
+      <p className="color-gray">{recentMessage.content}</p>
+      {!countUnreadMessage && (
+        <div className="text-right m-5">
+          <span className="data-unread">5</span>
+        </div>
+      )}
     </StyledChatItem>
   );
 }
@@ -18,15 +70,31 @@ ChatItem.propTypes = {
   chat: PropTypes.shape({
     id: PropTypes.string,
     members: PropTypes.arrayOf(PropTypes.string),
+    membersInfo: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string,
+        joinDate: PropTypes.shape({
+          nanoseconds: PropTypes.number,
+          seconds: PropTypes.number,
+        }),
+        nickname: PropTypes.string,
+        veganType: PropTypes.string,
+      }),
+    ),
     recentMessage: PropTypes.shape({
       content: PropTypes.string,
-      sentAt: PropTypes.number,
       sentBy: PropTypes.string,
       readBy: PropTypes.arrayOf(PropTypes.string),
+      sentAt: PropTypes.shape({
+        nanoseconds: PropTypes.number,
+        seconds: PropTypes.number,
+      }),
     }),
     type: PropTypes.number,
   }).isRequired,
   onChatClick: PropTypes.func.isRequired,
+  user: PropTypes.string.isRequired,
+  countUnreadMessage: PropTypes.number.isRequired,
 };
 
 export default ChatItem;
