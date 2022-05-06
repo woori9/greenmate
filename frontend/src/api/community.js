1af00ef7e94442b8f6aacfe4dbea7ccc4c4f1280
최시열 <continue75@naver.com>
2022-05-06 14:50:25 +0900

    fix: 🐛 [front] 커뮤니티 오타 수정

--- frontend/src/api/community.js
+++ frontend/src/api/community.js
@@ -2,6 +2,6 @@ import apiInstance from '../utils/apiInstance';
 
 /* 모임 목록 조회 */
 export default async function getFeedList() {
-  const response = await apiInstance.get('greenmates/community/feeds/');
+  const response = await apiInstance.get('/greenmates/community/feeds/');
   return response.data;
 }
