822e67cba6fe0af06449b0dd5c9fa9377da53cb0
woori9 <wrh95222@gmail.com>
2022-05-11 15:50:43 +0900

    feat: 🎸 [back] firebase-admin 추가, 알림 테스트 코드 추가

---
+++ backend/notifications/views/noti_wr.py
@@ -0,0 +1,26 @@
+import json
+from rest_framework.decorators import api_view
+from firebase_admin import messaging
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
+
+@api_view(['POST'])
+def test(request):
+    # This registration token comes from the client FCM SDKs.
+    data = json.loads(request.body)
+    registration_token = data['token']
+
+    # See documentation on defining a message payload.
+    message = messaging.Message(
+        data={
+            'title': '타이틀',
+            'body': '테스트입니다',
+        },
+        token=registration_token,
+    )
+
+    response = messaging.send(message)
+    print(response)
+    # Response is a message ID string.
+    print('Successfully sent message:', response)
+    return Response(status=HTTP_200_OK)
