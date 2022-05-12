b5b2f44b53936f905c5c4f9b840fa2b801488af5
박소미 <psomi0114@gmail.com>
2022-05-12 06:42:54 +0000

    Feature/모임 somi

--- backend/greenmates/serializers/evaluation.py
+++ backend/greenmates/serializers/evaluation.py
@@ -2,15 +2,22 @@ from rest_framework import serializers
 from ..models import UserReview, UserEvaluation
 
 class UserReviewPostSerializer(serializers.ModelSerializer):
+
+    def __init__(self, *args, **kwargs):
+        many = kwargs.pop('many', True)
+        super(UserReviewPostSerializer, self).__init__(many=many, *args, **kwargs)
+    
     class Meta:
         model = UserReview
         fields = '__all__'
         read_only_fields = ('me',)
-    
+
     def create(self, validated_data):
         validated_data['me'] = self.context['user']
+        mate_id = validated_data['mate'].id
+        evaluation_list = self.context['evaluations'][mate_id]
         user_review = UserReview.objects.create(**validated_data)
-        for evaluation in self.context['evaluation']:
+        for evaluation in evaluation_list:
             UserEvaluation.objects.create(
                 user_review = user_review,
                 evaluation=evaluation
