c95138e74f1b99b87b091ec795e8aa87c59008b5
최시열 <continue75@naver.com>
2022-05-13 00:16:27 +0000

    Feature/커뮤니티 siyeol

--- frontend/src/components/community/Feeds.jsx
+++ frontend/src/components/community/Feeds.jsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { getFeedList } from '../../api/community';
 import Feed from './Feed';
 
-function Feeds() {
+function Feeds({ categoryValue, vegeTypeValue }) {
   const [feeds, setFeeds] = useState([]);
-
+  // const [res, setRes] = useState([]);
   useEffect(() => {
     const getFeeds = async () => {
       const resData = await getFeedList();
@@ -12,9 +13,21 @@ function Feeds() {
     };
     getFeeds();
   }, []);
+
+  let res = feeds.filter(
+    feed => feed.category === categoryValue && feed.vege_type === vegeTypeValue,
+  );
+  if (categoryValue === 0) {
+    res = feeds.filter(feed => feed.vege_type === vegeTypeValue);
+  } else if (vegeTypeValue === 0) {
+    res = feeds.filter(feed => feed.category === categoryValue);
+  }
+  if (categoryValue === 0 && vegeTypeValue === 0) {
+    res = feeds;
+  }
   return (
     <div>
-      {feeds.map(feed => (
+      {res.map(feed => (
         <div key={feed.id}>
           <Feed feed={feed} />
         </div>
@@ -22,5 +35,10 @@ function Feeds() {
     </div>
   );
 }
+
+Feeds.propTypes = {
+  categoryValue: PropTypes.number.isRequired,
+  vegeTypeValue: PropTypes.number.isRequired,
+};
 
 export default Feeds;
