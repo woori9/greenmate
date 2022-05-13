4e8fddf70f08dac6334efa6172634970747d1762
최시열 <continue75@naver.com>
2022-05-13 17:57:37 +0900

    feat: 🎸 [front] 커뮤니티 기능 추가 구현

--- frontend/src/components/community/Comment.jsx
+++ frontend/src/components/community/Comment.jsx
@@ -1,39 +1,40 @@
-import { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import CardContent from '@mui/material/CardContent';
-import { getCommentList } from '../../api/community';
+// import { useState, useEffect } from 'react';
+// import PropTypes from 'prop-types';
+// import CardContent from '@mui/material/CardContent';
+// import { getCommentList } from '../../api/community';
 
-function Comment({ feedId }) {
-  const [comments, setComments] = useState([]);
-  useEffect(() => {
-    const getComments = async () => {
-      const resData = await getCommentList(feedId);
-      setComments(resData);
-    };
-    getComments();
-  }, []);
-  return (
-    <div>
-      {comments ? (
-        <CardContent>댓글이 없습니다.</CardContent>
-      ) : (
-        <CardContent>
-          {comments.map(comment => (
-            <div key={comment.id}>
-              <h1>{comment.id}</h1>
-              <p>{comment.nickname}</p>
-              <p>{comment.created_at}</p>
-              <p>{comment.is_like}</p>
-            </div>
-          ))}
-        </CardContent>
-      )}
-    </div>
-  );
-}
+// function Comment({ feedId }) {
+//   const [comments, setComments] = useState([]);
 
-Comment.propTypes = {
-  feedId: PropTypes.number.isRequired,
-};
+//   useEffect(() => {
+//     const getComments = async () => {
+//       const resData = await getCommentList(feedId);
+//       setComments(resData);
+//     };
+//     getComments();
+//   }, []);
+//   return (
+//     <div>
+//       {comments ? (
+//         <CardContent>댓글이 없습니다.</CardContent>
+//       ) : (
+//         <CardContent>
+//           {comments.map(comment => (
+//             <div key={comment.id}>
+//               <h1>{comment.id}</h1>
+//               <p>{comment.nickname}</p>
+//               <p>{comment.created_at}</p>
+//               <p>{comment.is_like}</p>
+//             </div>
+//           ))}
+//         </CardContent>
+//       )}
+//     </div>
+//   );
+// }
 
-export default Comment;
+// Comment.propTypes = {
+//   feedId: PropTypes.number.isRequired,
+// };
+
+// export default Comment;
