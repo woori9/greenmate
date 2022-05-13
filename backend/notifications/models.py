40506b4aa57681abb99feac797d9e4f988c472e8
백민아 <hoy8789@naver.com>
2022-05-13 00:39:47 +0000

    feat: 🎸 [back] 채팅 알람 api 구현

--- backend/notifications/models.py
+++ backend/notifications/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class FirebaseToken(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    registration_token = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
