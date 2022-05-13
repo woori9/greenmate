6ae22f6900c3ebda2c51a9958a2ca0c246b108cc
psomi0114 <psomi0114@gmail.com>
2022-05-13 14:52:03 +0900

    feat: 🎸 [back] 모임 시간 수정 및 취소 시 알람

--- backend/scheduler/moim.py
+++ backend/scheduler/moim.py
@@ -1,5 +1,7 @@
 import datetime
 from greenmates.models import Moim, Mate
+from notifications.views import send_message
+from notifications.models import FirebaseToken
 
 now = datetime.datetime.now()
 
@@ -12,7 +14,12 @@ def update_status():
     # 1. 모임 2시간 전인데, 모임 모집이 안된 경우 => moim status 0(모집중) -> 2(모집취소)
         ## mate_status 1(합류) -> 3(취소)
     Moim.objects.filter(time__lte=two_hrs, status=0).update(status=2)
-    Mate.objects.filter(mate_status=1, moim__status=2).update(mate_status=3)
+    mates =  Mate.objects.filter(mate_status=1, moim__status=2)
+    mates.update(mate_status=3)
+    tokens = FirebaseToken.objects.filter(user__mate__in=mates).distinct().values_list('registration_token', flat=True)
+    body = f'인원부족으로 취소된 모임이 있습니다.'
+    if tokens:
+        send_message(list(tokens), body)
 
     # 2. 모임 시간 후, moim status가 1(모집 완료)인 모임 => moim status 1(모집완료) -> 3(모임 종료)
         ## mate_status 1(합류) -> 4(완료)
