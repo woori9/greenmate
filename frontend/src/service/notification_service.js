af7a4a5adaae5ef802f3966ee77a279696eef59a
woori9 <wrh95222@gmail.com>
2022-05-16 10:58:15 +0900

    fix: 🐛 [front] notification_service 파일에서 삭제된 함수를 불러오는 import문 제

--- frontend/src/service/notification_service.js
+++ frontend/src/service/notification_service.js
@@ -1,6 +1,5 @@
 import { getMessaging, getToken, onMessage } from 'firebase/messaging';
 import app from './firebase';
-import { saveNotification } from './chat_service';
 import { sendToken } from '../api/notification';
 
 const messaging = getMessaging(app);
@@ -19,14 +18,22 @@ export const checkToken = async setTokenId => {
     }
   } catch (err) {
     setTokenId(null);
-    throw new Error('An error occurred while retrieving token. ', err);
+    throw new Error(err);
   }
 };
 
-export const onMessageListener = userId => {
+export const onMessageListener = handleOpenSnackbar => {
   return onMessage(messaging, payload => {
     const { data } = payload;
     console.log('Foreground Message', data);
-    saveNotification(userId, data);
+    const { body } = data;
+
+    const sampleNotification = {
+      id: '3',
+      title: body,
+      sentBy: '1',
+      createdAt: new Date().toLocaleDateString(),
+    };
+    handleOpenSnackbar(sampleNotification.title);
   });
 };
