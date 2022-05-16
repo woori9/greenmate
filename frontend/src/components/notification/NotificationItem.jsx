d83414bfd303428b6d98c91a92dcc56d8f4468d3
woori9 <wrh95222@gmail.com>
2022-05-16 11:01:35 +0900

    feat: 🎸 [front] 알림 클릭 이벤트 추가
    채팅방 관련 알림은 채팅방으로 이동

--- frontend/src/components/notification/NotificationItem.jsx
+++ frontend/src/components/notification/NotificationItem.jsx
@@ -1,6 +1,13 @@
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
 import vegan from '../../assets/vegan-icon.png';
+import {
+  findPrivateChatRoom,
+  getJoinDate,
+  getMoimChatRoom,
+} from '../../service/chat_service';
+import useUserInfo from '../../hooks/useUserInfo';
 
 const StyledNotificationItem = styled.div`
   display: flex;
@@ -10,7 +17,7 @@ const StyledNotificationItem = styled.div`
 
   .title {
     padding: 0rem 2rem;
-    max-width: 70%;
+    max-width: 60%;
     overflow: hidden;
     text-overflow: ellipsis;
     white-space: nowrap;
@@ -28,8 +35,46 @@ const StyledNotificationItem = styled.div`
 `;
 
 function NotificationItem({ notification }) {
+  const userInfo = useUserInfo();
+  const navigate = useNavigate();
+
+  const handleNotification = async target => {
+    if (target.type === 1) {
+      const chatRoom = await findPrivateChatRoom(
+        target.sentBy,
+        `${userInfo.id}`,
+      );
+      const joinDate = await getJoinDate(`${userInfo.id}`, chatRoom.id);
+      const notificationTargetId = target.sentBy;
+      const chatTitle = chatRoom.membersInfo[`nickname${target.sentBy}`];
+      navigate('/chatRoom', {
+        state: {
+          ...chatRoom,
+          joinDate,
+          notificationTargetId,
+          chatTitle,
+        },
+      });
+    }
+
+    if (target.type === 2) {
+      const chatRoom = await getMoimChatRoom(target.sentBy);
+      const joinDate = await getJoinDate(`${userInfo.id}`, chatRoom.id);
+      const chatTitle = '모임 타이틀';
+      const notificationTargetId = target.sentBy;
+      navigate('/chatRoom', {
+        state: {
+          ...chatRoom,
+          joinDate,
+          notificationTargetId,
+          chatTitle,
+        },
+      });
+    }
+  };
+
   return (
-    <StyledNotificationItem>
+    <StyledNotificationItem onClick={() => handleNotification(notification)}>
       <img src={vegan} alt="greenmate" />
       <p className="title">{notification.title}</p>
       <p className="date">{notification.createdAt}</p>
