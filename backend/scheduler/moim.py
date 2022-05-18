6f433a927aaaf12301e0b1f4d0b2067ba83b65b0
psomi0114 <psomi0114@gmail.com>
2022-05-19 00:23:28 +0900

    fix: 🐛 [back] 모임 취소 알람에 모임 title 포함
    모임 자동 취소 알람에  모임 title 보여주기

--- backend/scheduler/moim.py
+++ backend/scheduler/moim.py
@@ -2,6 +2,7 @@ import datetime
 from greenmates.models import Moim, Mate
 from notifications.views import send_message, create_multiple_alirm
 from notifications.models import FirebaseToken
+from django.db.models import Prefetch
 
 def make_messages(tokens, body):
     if tokens:
@@ -18,13 +19,14 @@ def update_status():
     two_hrs = now + datetime.timedelta(hours=2)
     # 1. 모임 2시간 전인데, 모임 모집이 안된 경우 => moim status 0(모집중) -> 2(모집취소)
         ## mate_status 1(합류) -> 3(취소)
-    Moim.objects.filter(time__lte=two_hrs, status=0).update(status=2)
-    mates =  Mate.objects.filter(mate_status=1, moim__status=2)
-    tokens = FirebaseToken.objects.filter(user__mate__in=mates).distinct().values_list('registration_token', flat=True)
-    body = f'인원부족으로 취소된 모임이 있습니다.'
-    make_messages(tokens, body)
-    mates.update(mate_status=3)
-    
+    moims = Moim.objects.filter(time__lte=two_hrs, status=0)
+    for moim in moims:
+        tokens = FirebaseToken.objects.filter(user__mate__moim_id=moim.id, user__mate__mate_status__lte=1).values_list('registration_token', flat=True)
+        body=f'[{moim.title}] 모임이 인원부족으로 취소되었습니다.'
+        make_messages(tokens, body)
+    moims.update(status=2)
+    Mate.objects.filter(mate_status=1, moim__status=2).update(mate_status=3)
+
     # 2. 모임 시간 후, moim status가 1(모집 완료)인 모임 => moim status 1(모집완료) -> 3(모임 종료)
         ## mate_status 1(합류) -> 4(완료)
     Moim.objects.filter(time__lte=now, status=1).update(status=3)
