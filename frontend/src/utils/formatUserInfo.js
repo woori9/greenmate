1f8c282627394d89daabab9e78560b8913c6ed98
woori9 <wrh95222@gmail.com>
2022-05-18 21:41:40 +0900

    fix: 🐛 [front] vegeType이 0일 때 모임 채팅 생성 오류 fix

--- frontend/src/utils/formatUserInfo.js
+++ frontend/src/utils/formatUserInfo.js
@@ -1,10 +1,10 @@
 /* eslint-disable camelcase */
 export default function formatUserInfo(userInfo) {
-  const { id, nickname, vege_type, vegeType } = userInfo;
+  const { id, nickname, vege_type } = userInfo;
   const formattedInfo = {
     id: `${id}`,
     nickname,
-    vegeType: vege_type || vegeType,
+    vegeType: vege_type,
   };
   return formattedInfo;
 }
